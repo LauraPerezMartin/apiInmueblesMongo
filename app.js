94b9a07ef1a297ac373cc0d7cba4a8f8203d60c2
Laura Perez <laurap48@hotmail.com>
2023-05-17 10:03:02 +0200

    Eliminar rutas y archivos de rutas que no utilizo
    Eliminar archivos routes/index.js y routes/users.js y quitarlos de app.js

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const apiRouter = require('./routes/api');
 
 const app = express();
@@ -17,8 +15,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 
 module.exports = app;
